e3a2092de97f561dbb57ae003dbf5772263560de
Davis Asano <dfasano@iastate.edu>
2021-05-29 15:03:49 -0700

    added code to display number of sentences in a string and print each

--- one ex per file/ex2.py
+++ one ex per file/ex2.py
@@ -8,6 +8,11 @@
 print("start of part 2") # set breakpoint here
 s = "Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects."
 # your code here
+n = s.split(".") #split s at each .
+
+print("there are ", len(n), " sentences: ") #display how many sentences there are
+for e in n: #for each sentence
+    print(e) #print sentence
 
 print("end of 2") # set breakpoint here 
 '''
