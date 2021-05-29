49dc3ca108bc4b7adb07e0ae24634d74b0f46ded
Davis Asano <dfasano@iastate.edu>
2021-05-29 15:03:31 -0700

    Created new list via indexing and slicing

--- one ex per file/ex1.py
+++ one ex per file/ex1.py
@@ -2,9 +2,11 @@
 # task:  L = [0, [], [1,2,3,4], [[5],[6,7]], [8,9,10]]
 # using both, indexing and slicing on L, assemble and print a new list N that contains:
 # [0,2,3,[5,6],8,10] 
+#    
 # as an example, here's  how i've constructed a similar list X which contains [[2, 3], [10]] from L:
-#
+#     0    1   2          3            4
 # L = [0, [], [1,2,3,4], [[5],[6,7]], [8,9,10]]
+#              0 1 2 3    0    1       0 1 2
 # X = [ L[2][1:-1], L[-1][2] ] 
 # print(x)  => [[2, 3], [10]]
 # You need to do something similar but end up with [0,2,3,[5,6],8,10] instead. One way to work 
@@ -14,9 +16,11 @@ print("start of part 1") # set breakpoint here
 L = [0, [], [1,2,3,4], [[5],[6,7]], [8,9,10]]
 print(L)
 # your code
-
-
+N = [L[0],L[2][1],L[2][2],[L[3][0][0],L[3][1][0]],L[4][0],L[4][2]] # create new list [0,2,3,[5,6],8,10] 
+print(N)
 print("end of 1") # set breakpoint here 
+
+
 '''
 
 
